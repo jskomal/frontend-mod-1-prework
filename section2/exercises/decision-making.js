214ce77dd83dc4bb8eb83f7da93538d9086e5514
Jordan Skomal <jskomal@gmail.com>
2021-10-25 14:14:27 -0600

    Complete decision-making.js, functions.js, if-statments.js, and reflections.md

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -38,16 +38,28 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
-
+You have been prompted to pick between doors 1 and 2. this if statement declares that if you pick
+door 1, the program will use the value 'hat' for the bear's clothing. if the user picks any
+other number, the program will use the string 'scarf' for the bear's clothing.
 2. What variable has a new value assigned to it after the first if statement executes?
-
+bearChoice - defined in line 10 originally, bearChoice is set to 1 by default, and would be
+redefined by the user's input with the rules specified in lines 27-35
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
-
+"scarf" - as the else covers all numbers that are not 1.
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+This is an if statement that takes user's input of choice of options (1,2,3) and outputs a different solution
+based on the variable state. if the bearChoice is set to 1, the console returns
+"You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!"
+if you change bearChoice to 2, it returns "You tell the bear the " + bearClothing + " is too small and it starts to cry!"
+if you change bearChoice to 3, it returns "You run as fast as you can into the next room. It's full of snakes!"
+and if you change bearChoice to anything that is not 1, 2, or 3, it will return "You stay with the bear and become it's best friend!"
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
-
+"You run as fast as you can into the next room. It's full of snakes!"
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-
+"You tell the bear the hat is too small and it starts to cry!"
 7. What is your favorite ending?
+I prefer the final else statement, staying with the bear and becoming best friends,
+as I find it a good reminder to limit users inputs to guide their path better, but this still happens if everything goes wrong
+
 */
