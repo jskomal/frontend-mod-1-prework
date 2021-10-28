db4fd0b647cd99595b88d5a1861ff2329862611c
Jordan Skomal <jskomal@gmail.com>
2021-10-28 13:30:07 -0600

    Add Final Prep Work

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,41 +2,60 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// declares function buildABear with the parameters name, age, fur, clothes, and specialPower
 function buildABear(name, age, fur, clothes, specialPower) {
+  //declares varaiable greeting which greets and uses it's own name via interpolation
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  // declares a variable demographics which contains an array
   var demographics = [name, age];
+  // declares variable powerSaying which tells about it's specialPower via concatenation
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  // declares variable builtBear which is an object containg key value pairs of various data
   var builtBear = {
+    // sets up a dynamic value for demographics, using the var array created earlier
     basicInfo: demographics,
+    //sets up a dynamic value for clothes, that is specified in the function's 4th argument
     clothes: clothes,
+    // sets up a dynamic value for fur, that is specified in the function's 3rd argument
     exterior: fur,
+    //sets a static value for the cost
     cost: 49.99,
+    //creates an array with two dynamic variables, and one static phrase
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+    //sets a static value for the cuddlyness
     isCuddly: true,
   };
-
+//returns builtBear, the object on starting on line 14
   return builtBear
 }
-
+// calls function buildABear with specifed arguments linked to the parameters on line 5, nothing is stored
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+// same but with different answers for the arguments
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
+// declare function fizzBuzz with parameters num1, num2, and range
 function fizzBuzz(num1, num2, range) {
+  // loop this with initialExpression = 0 and adding 1 to the initialExpression each loop as many times as is specified in the range argument
   for (var i = 0; i <= range; i++) {
+    // if the remainder of i divided by num1 (the first argument) is 0 AND the remainder of i divided by num2 (the second argument) is 0, print 'fizzbuzz' to the console
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz');
+      // if either of those is false, check if the remainder of i divided by num1 (the first argument) is 0, if so print 'fizz' to the console
     } else if (i % num1 === 0) {
       console.log('fizz');
+      // if that too is false, check if the remainder of i divided by num2 (the second argument) is 0, if so print 'buzz' to the console
     } else if (i % num2 === 0) {
       console.log('buzz');
+      // if all so far return false, log the current value of i to the console
     } else {
       console.log(i);
     }
   }
 }
-
+// correctly runs the function 100 times
 fizzBuzz(3, 5, 100);
+// does not run because the b in fizzbuzz is not capitalized correctly
 fizzbuzz(5, 8, 400);
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,41 +1,66 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
-
+var heroName =  'BetterGuy';
+var specialAbility = 'add egg wash on your baked goods for better browning';
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
-
+var greeting = 'Ho there! I am ' + heroName +'!';
+var catchphrase = `Are you mad? Putting those in the oven like that? Stop right now so I can ${specialAbility}!`;
 // Declare two variables - power AND energy - set to integers
-
+var power = 9000;
+var energy = 100;
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
-
+var fullPower = power * 500;
+var fullEnergy = energy + 150;
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
+var isHuman = true;
+var identityConcealed = true;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
-
+var archEnemies = ['Limp Toast man', 'Eggs are Evil Boy', 'Soggy Biscuit Samuel']
+var sidekicks = ['Butterman', 'The Whipped Cream Witch', 'Gelato Gal']
 // Print the first sidekick to your console
-
+console.log(sidekicks[0]);
 // Print the last archEnemy to the console
-
+console.log(archEnemies[2]);
 // Write some code to add a new archEnemy to the archEnemies array
-
+archEnemies.push('The Wicked Waffle');
 // Print the archEnemies array to console to ensure you added a new archEnemy
-
+console.log(archEnemies)
 // Remove the first sidekick from the sidekicks array
-
+sidekicks.shift();
 // Print the sidekicks array to console to ensure you added a new sidekick
-
+console.log(sidekicks);
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+var dangerLevel
+var saveTheDay = 'Maillard smiles upon us today!'
+var badExcuse = 'YOUR DOUGH IS TOO WET, FOOL! This will never work!'
 
+// there is no need for the two other arguments, but it could be written as:
+// function assessSituation(dangerLevel, saveTheDay, badExcuse){
+function assessSituation(dangerLevel){
+  if (dangerLevel > 50) {
+    console.log(badExcuse)
+  }
+  else if (dangerLevel >=10) {
+    console.log(saveTheDay)
+  }
+  else if (dangerLevel < 10) {
+    console.log('Meh. Hard pass.')
+  }
+}
+assessSituation(99);
+assessSituation(21);
+assessSituation(3);
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
@@ -56,16 +81,49 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
-
+var scaryMonster = {
+  name: 'Beth',
+  smell: 'sickly sweet',
+  weight: 3002,
+  citiesDestroyed: ['Montpillier, VT', 'Rifle, CO', 'Limon, CO'],
+  luckyNumbers: [1,4,95],
+  address: {
+    number: 626,
+    street: 'Monster way',
+    state: 'CO',
+    zip: 80111
+  }
+};
+console.log(scaryMonster);
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+class Superhero {
+  constructor(name, superpower, age, archNemesis, powerLevel, energyLevel) {
+  this.name = name;
+  this.superpower = superpower;
+  this.age = age;
+  this.archNemesis = 'The Syntax Error';
+  this.powerLevel = 100;
+  this.energyLevel = 50;
+  }
+
+sayName() {
+  console.log(this.name)
+}
+maximizeEnergy() {
+  this.energyLevel = 1000
+  }
+gainPower(addPowerNum) {
+  this.powerLevel = this.powerLevel + addPowerNum
+  }
+};
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
@@ -73,12 +131,15 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
 
 // - Create 2 instances of your SuperHero class
-
-
+var ferretGirl = new Superhero('Ferret Girl', 'is a ferret at all times', 35);
+console.log(ferretGirl);
+var geriatricGamer = new Superhero('The Geriatric Gamer', 'is still competitive at video games', 97);
+console.log(geriatricGamer);
+ferretGirl.gainPower(1000);
+console.log(ferretGirl);
 // Reflection
-// What parts were most difficult about this exerise?
+// What parts were most difficult about this exerise? Remembering the specific syntax for each different facet, Objects vs functions vs classes, I had to look a couple of minor details up, which slowed me down.
 
-// What parts felt most comfortable to you?
+// What parts felt most comfortable to you? I feel very comfortable with the fundamentals, but putting them into practice means taking more time to engrain these into my brain
 
-// What skills do you need to continue to practice before starting Mod 1?
-
+// What skills do you need to continue to practice before starting Mod 1? All of them! the more comfortable I am with all of them, the more quickly I can implement these into real world scenarios!
